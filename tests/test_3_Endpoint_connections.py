fb79cc2344888ae92ee0f5300315d8614ed1058e
Alexander <alexander.bergkvist@yh.nackademin.se>
2023-11-03 15:28:29 +0100

    Created new test and modified/removed unnesary files

--- tests/test_3_Endpoint_connections.py
+++ tests/test_3_Endpoint_connections.py
@@ -5,16 +5,14 @@ import ssl
 context = ssl._create_unverified_context()
 
 
-def test_Is_online_index():
-    assert urllib.request.urlopen("http://127.0.0.1:5000", context=context, timeout=10)
+def test_is_online_index():
+    assert urllib.request.urlopen("http://127.0.0.1:5000", context=context, timeout=10) #Test if localhost is up and running.
 
-def test_Is_online_form():
-    assert urllib.request.urlopen("http://127.0.0.1:5000/form", context=context, timeout=10)
+def test_is_online_form():
+    assert urllib.request.urlopen("http://127.0.0.1:5000/priser", context=context, timeout=10) # Testing if the priser page is up and running.
 
-def test_Confirm_HTTPError_on_api_GET():
+def test_confirm_HTTPError_on_api_GET():
     with pytest.raises(urllib.request.HTTPError):
-        urllib.request.urlopen("http://127.0.0.1:5000/api", context=context, timeout=10)
+        urllib.request.urlopen("http://127.0.0.1:5000/api", context=context, timeout=10) # Makes sure that this page doesn't exist.
 
-def test_Confirm_no_error_on_api_POST():
-    with pytest.raises(urllib.request.HTTPError):
-        urllib.request.urlopen("http://127.0.0.1:5000/api", context=context, timeout=10)
+#Taken from teacher, are multiple test worth using.
